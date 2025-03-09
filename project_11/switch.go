e7ad4f6617902fc35a16738025f6cb8d3425e59b
siva kumar reddy bojja <sivakumarreddybojja@sivas-MacBook-Pro.local>
2025-03-09 22:30:27 +0530

    switch statement

---
+++ project_11/switch.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+func main() {
+    var accountBalance float64= 1000
+	    // Important: we have only for loop in go.
+		// example to run the code for 2 iterations with an condition.
+		// for i:=0; i < 2; i++ {
+		// to run in infinite loop we don't need to specify the condition.
+		fmt.Println("Welcome to Go Bank")
+        for {
+			
+			fmt.Println("what you want to do?")
+			fmt.Println("1. Deposit")
+			fmt.Println("2. Withdraw")
+			fmt.Println("3. Check Balance")
+	
+			var choice int
+			fmt.Println("Your choice:")
+			fmt.Scanln(&choice)
+
+		switch choice {
+
+			case 1:
+				fmt.Println("Your Deposit:")
+			var deposit float64
+			fmt.Scanln(&deposit)
+			if deposit < 0 {
+				fmt.Println("Invalid deposit, must be greater than 0")
+				// naked return, it stops execution.
+				// return
+				continue
+			}
+			accountBalance += deposit
+			fmt.Println("Your current balance is:", accountBalance)
+
+
+			case 2:
+				fmt.Println("Withdraw")
+			var withdraw float64
+			fmt.Scanln(&withdraw)
+			if withdraw < 0 {
+				fmt.Println("Invalid withdraw, must be greater than 0")
+				// return
+				continue
+			}
+			if withdraw > accountBalance {
+				fmt.Println("Insufficient balance")
+			} else {
+				accountBalance -= withdraw
+				fmt.Println("Your current balance is:", accountBalance)
+			}
+
+			case 3:
+				fmt.Println("Your current balance is:", accountBalance)
+
+			default:
+				fmt.Println("Invalid choice")
+				fmt.Println("Thanks for choosing our bank")
+		        fmt.Println("Have a nice day!")
+				// can't use break here it can't take outside the loop, instead use return
+				// break
+				return
+			}
+		}
+}
