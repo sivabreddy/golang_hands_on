8f1302d5b6adcbf3fa7fb2a278e54e17937dfbf1
siva kumar reddy bojja <sivakumarreddybojja@sivas-MacBook-Pro.local>
2025-03-11 18:09:36 +0530

    exposing_packages_meth0ds_constructors

--- project_20_constructor_functions/constructor.go
+++ project_20_constructor_functions/constructor.go
@@ -1,45 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+	"example.com/constructor/user"
 )
-
-// we can define struct inside or ourside the function
-// Use first character uppercase to make it public to access in other packages
-type User struct {
-	firstName string
-	lastName  string
-	birthdate string
-	//Time is also struct provided by the time package
-	createdAt time.Time
-}
-// function attached to a struct is called method
-// receiver is the variable that is used to call the method
-func (u *User) outputUserDetails() { 
-    fmt.Println(u.firstName, u.lastName, u.birthdate,u.createdAt)
-}
-// replace the first and last name with empty string in the struct
-func (u *User) clearUserName() {
-   u.firstName = ""
-   u.lastName = ""
-}
-
-// creations or constructor function , returns a pointer
-func newUser(userFirstName, userLastName, userBirthdate string) (*User, error){
-	//validations
-	if userFirstName == "" || userLastName == "" || userBirthdate == "" {
-		return nil, 
-		errors.New("first name, last name and birthdate are required")
-	}
-	return &User {
-		firstName: userFirstName,
-		lastName: userLastName,
-		birthdate: userBirthdate,
-		createdAt: time.Now(), // comma is needed even if it is a last member
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
@@ -47,9 +11,9 @@ func main() {
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
     // Here the "appUser" is a variable of type User
-	var appUser *User
+	var appUser *user.User
 	
-    appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+    appUser, err := user.NewUser(userFirstName, userLastName, userBirthdate)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -57,9 +21,9 @@ func main() {
 
 
     // Here we are calling the method on the appUser variable
-	appUser.outputUserDetails() //output details
-	appUser.clearUserName() // empty firstName and lastName
-	appUser.outputUserDetails() //output details again
+	appUser.OutputUserDetails() //output details
+	appUser.ClearUserName() // empty firstName and lastName
+	appUser.OutputUserDetails() //output details again
 
 }
 
---
+++ project_20_constructor_functions/user/user.go
@@ -0,0 +1,43 @@
+package user
+
+import (
+	"fmt"
+	"time"
+	"errors"
+)
+
+// we can define struct inside or ourside the function
+// Use first character uppercase to make it public to access in other packages
+type User struct {
+	FirstName string
+	LastName  string
+	Birthdate string
+	//Time is also struct provided by the time package
+	CreatedAt time.Time
+}
+// function attached to a struct is called method
+// receiver is the variable that is used to call the method
+func (u *User) OutputUserDetails() { 
+    fmt.Println(u.FirstName, u.LastName, u.Birthdate, u.CreatedAt)
+}
+// replace the first and last name with empty string in the struct
+func (u *User) ClearUserName() {
+   u.FirstName = ""
+   u.LastName = ""
+}
+
+
+// creations or constructor function , returns a pointer
+func NewUser(userFirstName, userLastName, userBirthdate string) (*User, error){
+	//validations
+	if userFirstName == "" || userLastName == "" || userBirthdate == "" {
+		return nil, 
+		errors.New("first name, last name and birthdate are required")
+	}
+	return &User {
+		FirstName: userFirstName,
+		LastName: userLastName,
+		Birthdate: userBirthdate,
+		CreatedAt: time.Now(), // comma is needed even if it is a last member
+	}, nil
+}
