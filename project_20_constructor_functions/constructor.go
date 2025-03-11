6bfa4026ca5c949d1a95342402eaddab1c6159cf
siva kumar reddy bojja <sivakumarreddybojja@sivas-MacBook-Pro.local>
2025-03-11 18:12:11 +0530

    updated

--- project_20_constructor_functions/constructor.go
+++ project_20_constructor_functions/constructor.go
@@ -27,6 +27,14 @@ func main() {
 
 }
 
+// output
+// sivas-MacBook-Pro project_20_constructor_functions % ./constructor 
+// Please enter your first name: siva
+// Please enter your last name: bojja
+// Please enter your birthdate (MM/DD/YYYY): 11/01/1988
+// siva bojja 11/01/1988 2025-03-11 18:10:05.869602 +0530 IST m=+9.222879668
+//   11/01/1988 2025-03-11 18:10:05.869602 +0530 IST m=+9.222879668
+
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
